ed2fb593cab1a309ab7fd11985b5548bb7385710
fairfax <lovelace.af@gmail.com>
2021-09-29 23:20:11 -0400

    managed to get button propagation working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
 //checking to see date
 const today = moment();
 console.log(today.format());
+checkTime();
+
+//set event bubbling for btn
+var wrapper = document.querySelector('.container');
+wrapper.addEventListener('click', (event) => {
+    event.stopPropagation();
+
+    // Grab the USER input
+    var task = event.target.parentNode.previousElementSibling.value;
+    console.log(`Task: ${task}`);
+
+    // set our new data
+    let timeTask = event.target.parentNode.previousElementSibling.getAttribute('id');
+    console.log(timeTask);
+
+    localStorage.setItem(timeTask, JSON.stringify(task));
+})
 
 //sets the current day and date in the header
 function checkTime () {
@@ -16,47 +33,38 @@ function checkTime () {
     var nineAmInput = document.getElementById("9am");
     nineAmInput.textContent = nineAm;
 
-    var thisHour = moment().format("HH");
     var tenAm = localStorage.getItem("10am");
     var tenAmInput = document.getElementById("10am");
     tenAmInput.textContent = tenAm;
 
-    var thisHour = moment().format("HH");
     var elevenAm = localStorage.getItem("11am");
     var elevenAmInput = document.getElementById("11am");
     elevenAmInput.textContent = elevenAm;
 
-    var thisHour = moment().format("HH");
     var twelvePm = localStorage.getItem("12pm");
     var twelvePmInput = document.getElementById("12pm");
     twelvePmInput.textContent = twelvePm;
 
-    var thisHour = moment().format("HH");
     var onePm = localStorage.getItem("1pm");
     var onePmInput = document.getElementById("1pm");
     onePmInput.textContent = onePm;
 
-    var thisHour = moment().format("HH");
     var twoPm = localStorage.getItem("2pm");
     var twoPmInput = document.getElementById("2pm");
     twoPmInput.textContent = twoPm;
 
-    var thisHour = moment().format("HH");
     var threePm = localStorage.getItem("3pm");
     var threePmInput = document.getElementById("3pm");
     threePmInput.textContent = threePm;
 
-    var thisHour = moment().format("HH");
     var fourPm = localStorage.getItem("4pm");
     var fourPmInput = document.getElementById("4pm");
     fourPmInput.textContent = fourPm;
 
-    var thisHour = moment().format("HH");
     var fivePm = localStorage.getItem("5pm");
     var fivePmInput = document.getElementById("5pm");
     fivePmInput.textContent = fivePm;
 
-    var thisHour = moment().format("HH");
     var sixPm = localStorage.getItem("6pm");
     var sixPmInput = document.getElementById("6pm");
     sixPmInput.textContent = sixPm;
@@ -64,7 +72,7 @@ function checkTime () {
     //variable for save button
     var saveButton = document.querySelector(".saveBtn");
     
-    //sets
+    //sets the colours depending on the time
     var userTextInput = document.querySelectorAll("textarea");
     var timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"]
     for(i=0; i< userTextInput.length; i++) {
@@ -78,25 +86,4 @@ function checkTime () {
         }
            
     };
-
-    saveButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        console.log(saveButton, "hello");
-    
-        var userTasks = {
-           // task: document.getElementsByClassName("description").val()
-           task: document.getElementById("9am").value
-        }
-    
-        console.log(userTasks);
-    
-        var userInput = localStorage.getItem("9am");
-        console.log(userInput);
-     //   var JSONInput = JSON.parse(userInput);
-     //   console.log(JSONInput);
-     //   JSONInput.push(userTasks);
-    
-        localStorage.setItem('9am', JSON.stringify(userTasks.task));
-    });
-
     
